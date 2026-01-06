788d42b471f40d218e6cc744f6bbfaeaa8c1c3ca
Niloy Barua <158665843+Niloy2267@users.noreply.github.com>
2026-01-06 20:03:29 +0600

    Add files via upload

---
+++ Cp-3./problem1.py
@@ -0,0 +1,4 @@
+a = "niloy"
+print(a[0:3])
+character=a[4]
+print(character)
---
+++ Cp-3./problem2.py
@@ -0,0 +1,7 @@
+name ="niloy"
+print(name[0:3])
+print(name[-3:-1])
+print(name[1:3])
+print(name[:4])
+print(name[1:])
+print(name[1:5])
