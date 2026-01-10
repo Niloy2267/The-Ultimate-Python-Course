f12c6ff5912881fb77d2a8dd78df575cd87a350c
Niloy Barua <158665843+Niloy2267@users.noreply.github.com>
2026-01-10 21:09:09 +0600

    Add files via upload

---
+++ Cp-6/problem11.py
@@ -0,0 +1,6 @@
+post = input("Enter the post: ")
+
+if "niloy" in post.lower():   # small letter
+    print("This post is talking about Niloy")
+else:
+    print("This post is not talking about Niloy")
